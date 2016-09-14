efdc3aa83f337ba45b18ee4d5f522047eb3ae108
flowmemo <flowmemo@outlook.com>
2016-09-14 19:56:50 +0800

    303. Range Sum Query - Immutable

---
+++ 303.js
@@ -0,0 +1,53 @@
+/*
+303. Range Sum Query - Immutable
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+*/
+
+/**
+ * @constructor
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  const len = nums.length
+  const sum = Array(len + 1)
+  sum[0] = 0
+  for (let i = 0; i < len; i++) {
+    sum[i + 1] = sum[i] + nums[i]
+  }
+  this.sum = sum
+}
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (i, j) {
+  return this.sum[j + 1] - this.sum[i]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var numArray = new NumArray(nums);
+ * numArray.sumRange(0, 1);
+ * numArray.sumRange(0, 2);
+ */
+
+let tc = [0, 1, 2, 3, 4, 5]
+let numArray = new NumArray(tc)
+for (let i = 0; i < tc.length; i++) {
+  let a = (0 + i) * (i + 1) / 2
+  let r = numArray.sumRange(0, i)
+  console.log(a === r, a, r)
+}
+
