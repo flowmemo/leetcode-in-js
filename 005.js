917f6a14ed9a325cd4380c15f8ff657e44a4dc57
flowmemo <flowmemo@outlook.com>
2016-11-19 23:16:33 +0800

    5. Longest Palindromic Substring

---
+++ 005.js
@@ -0,0 +1,71 @@
+/*
+5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  const len = s.length
+  let rstart = 0
+  let rstep = 0
+  for (let i = 1; i < 2 * len; ++i) {
+    let j = (i - 1) >> 1
+    let k = i >> 1
+    let [ts, tend] = maxP(s, j, k)
+    if (tend - ts > rstep) {
+      rstart = ts
+      rstep = tend - ts
+    }
+  }
+
+  return s.slice(rstart, rstart + rstep)
+}
+
+function maxP (s, j, k) {
+  const len = s.length
+  while (j >= 0 && k < len) {
+    if (s[j] !== s[k]) {
+      break
+    }
+    --j
+    ++k
+  }
+
+  return [j + 1, k]
+}
+
+let tc = [
+  'babad',
+  'cbbd',
+  'a',
+  'abc'
+]
+
+let ans = [
+  'bab',
+  'bb',
+  'a',
+  'a'
+]
+
+for (let i = 0; i < tc.length; ++i) {
+  let a = ans[i]
+  let r = longestPalindrome(tc[i])
+  console.log(a === r, a, r)
+}
