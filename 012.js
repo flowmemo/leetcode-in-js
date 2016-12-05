1b92d432dd68f5543e8f9da35d6356bf16e82081
flowmemo <flowmemo@outlook.com>
2016-12-05 22:55:34 +0800

    12. Integer to Roman 13. Roman to Integer

---
+++ 012.js
@@ -0,0 +1,18 @@
+/*
+12. Integer to Roman
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  const s1 = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+  const s2 = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+  const s3 = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+  const s4 = ['', 'M', 'MM', 'MMM']
+  return s4[num / 1000 | 0] + s3[(num % 1000) / 100 | 0] + s2[(num % 100) / 10 | 0] + s1[num % 10]
+}
---
+++ 013.js
@@ -0,0 +1,29 @@
+/*
+13. Roman to Integer
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const len = s.length
+  const m = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+  let res = 0
+  for (let i = len - 1; i >= 0; --i) {
+    if (i < len - 1 && m[s[i]] < m[s[i + 1]]) res -= m[s[i]]
+    else res += m[s[i]]
+  }
+  return res
+}
