0a270e2dc40721691450857ae807c1829badc374
flowmemo <flowmemo@outlook.com>
2016-12-06 22:45:57 +0800

    16. 3Sum Closest

---
+++ 016.js
@@ -0,0 +1,52 @@
+/*
+16. 3Sum Closest
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+  const n = nums.length
+  let res = nums[0] + nums[1] + nums[2]
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < n - 2; ++i) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+    let j = i + 1
+    let k = n - 1
+    const rest = target - nums[i]
+    while (j < k) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) ++j
+      else if (k < n - 1 && nums[k] === nums[k + 1]) --k
+      else {
+        const sum2 = nums[j] + nums[k]
+        if (Math.abs(rest - sum2) < Math.abs(res - target)) res = nums[i] + sum2
+        if (sum2 < rest) ++j
+        else if (sum2 > rest) --k
+        else return target
+      }
+    }
+  }
+  return res
+}
+
+let tc = [
+  [[-10, 0, -2, 3, -8, 1, -10, 8, -8, 6, -7, 0, -7, 2, 2, -5, -8, 1, -4, 6],
+    18]
+]
+
+let ans = [
+  17
+]
+
+for (let i = 0; i < tc.length; ++i) {
+  let r = threeSumClosest(...tc[i])
+  let a = ans[i]
+  console.log(a === r, a, r)
+}
