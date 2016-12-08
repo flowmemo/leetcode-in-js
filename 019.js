c80609528abad22a7edfa9e7d619a923d72c077b
flowmemo <flowmemo@outlook.com>
2016-12-08 18:43:37 +0800

    19. Remove Nth Node From End of List

---
+++ 019.js
@@ -0,0 +1,40 @@
+/*
+19. Remove Nth Node From End of List
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/** one-pass.
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  const dummy = {}
+  dummy.next = head
+  let cur = head
+  let pre = dummy
+  for (let i = 0; i < n; ++i) cur = cur.next
+  while (cur != null) {
+    cur = cur.next
+    pre = pre.next
+  }
+  pre.next = pre.next.next
+  return dummy.next
+}
