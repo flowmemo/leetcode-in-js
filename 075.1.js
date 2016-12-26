dcfb83fdc3c9da8219464729897ce45c170dea1b
flowmemo <flowmemo@outlook.com>
2016-12-26 23:14:24 +0800

    75. Sort Colors

---
+++ 075.1.js
@@ -0,0 +1,33 @@
+/*
+75. Sort Colors
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+*/
+
+/** dutch flag sort
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const swap = require('./algorithm.js').swap
+var sortColors = function (nums) {
+  let left = 0
+  let right = nums.length
+  let i = 0
+  while (i < right) {
+    if (nums[i] < 1) {
+      swap(nums, i, left++)
+    } else if (nums[i] > 1) {
+      swap(nums, i, --right)
+    } else ++i
+  }
+}
---
+++ 075.2.js
@@ -0,0 +1,34 @@
+/*
+75. Sort Colors
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+*/
+
+/** counting sort
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  const count = Array(3).fill(0)
+  for (let n of nums) {
+    count[n]++
+  }
+  let ci = 0
+  let i = 0
+  while (i < nums.length) {
+    if (count[ci]) {
+      nums[i++] = ci
+      count[ci]--
+    } else ci++
+  }
+}
