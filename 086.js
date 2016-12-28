adda1893f8f970dac3c6b6332f77adaf11904f43
flowmemo <flowmemo@outlook.com>
2016-12-28 22:50:05 +0800

    86. Partition List

---
+++ 086.js
@@ -0,0 +1,44 @@
+/*
+86. Partition List
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  const h1 = {}
+  const h2 = {}
+  let less = h1
+  let other = h2
+  while (head != null) {
+    if (head.val < x) {
+      less.next = head
+      less = less.next
+    } else {
+      other.next = head
+      other = other.next
+    }
+    head = head.next
+  }
+  other.next = null
+  less.next = h2.next
+  return h1.next
+}
+
+module.exports = partition
---
+++ test/fixtures/086.js
@@ -0,0 +1,8 @@
+const genLinkedList = require('../genLinkedList.js')
+
+module.exports = [
+  {
+    input: [genLinkedList([1,4,3,2,5,2]), 3],
+    ans: genLinkedList([1,2,2,4,3,5])
+  }
+]
