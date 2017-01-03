a0f3f004a2e3a1b2539042b77e4f34cc7b5eca36
flowmemo <flowmemo@outlook.com>
2017-01-03 23:19:51 +0800

    new check.js

--- test/check.js
+++ test/check.js
@@ -1,13 +1,7 @@
 const assert = require('assert')
-const util = require('util')
 
 const file = process.argv[2]
 const solution = require(`../${file}`)
 const id = file.slice(0, 3)
-const fixtures = require(`./fixtures/${id}.js`)
-for (const {input, ans} of fixtures) {
-  const output = solution(...input)
-  const errMsg = 'input is :\n' + util.inspect(input, {colors: true}) + '\noutput is :\n' +
-    util.inspect(output,{colors: true}) + '\nans is :\n' +  util.inspect(ans,{colors: true})
-  assert.deepStrictEqual(output, ans, errMsg)
-}
+const {data, checker, options} = require(`./fixtures/${id}.js`)
+checker(solution, data, options)
