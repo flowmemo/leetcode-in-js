5889ae8225901a7e8cb35b595ed7c3783bf0744c
flowmemo <flowmemo@outlook.com>
2017-01-04 20:07:06 +0800

    add algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,96 @@
+'use strict'
+function less (a, b) {
+  return a < b
+}
+ 
+function greater (a, b) {
+  return a > b
+}
+ 
+/**
+ * Finds the position of the first element in an ordered range that has a value not less than specified value.
+ * @param {Array} arr - array to search
+ * @param {number} first - beginning index to search
+ * @param {number} last - past-the-end index to search.
+ * @param {number} val - value to compare the elements to.
+ * @param {function} [comp] - custom comparator. If ommited then `<` is used.
+ * @return {number} index - pointing to the first element that is not less than value, or `last` if no such element is found.
+ */
+function lowerBound (arr, first, last, val, comp = less) {
+  let count = last - first
+  while (count !== 0) {
+    const half = count >> 1
+    const mid = first + half
+    if (comp(arr[mid], val)) {
+      first = mid + 1
+      count -= half + 1
+    } else {
+      count = half
+    }
+  }
+  return first
+}
+ 
+/**
+ * Finds the position of the first element in an ordered range that has a value that is greater than a specified value.
+ * @param {Array} arr - array to search
+ * @param {number} first - beginning index to search
+ * @param {number} last - past-the-end index to search.
+ * @param {number} val - value to compare the elements to.
+ * @param {function} [comp] - custom comparator.
+ * @return {number} index - pointing to the first element that is greater than value, or `last` if no such element is found.
+ */
+function upperBound (arr, first, last, val, comp = less) {
+  let count = last - first
+  while (count !== 0) {
+    const half = count >> 1
+    const mid = first + half
+    if (comp(val, arr[mid])) {
+      count = half
+    } else {
+      first = mid + 1
+      count -= half + 1
+    }
+  }
+  return first
+}
+ 
+/**
+ * Determin whether [first, last) of sorted `arr` contains `val`
+ * @param {Array} arr - array to search
+ * @param {number} first - beginning index to search
+ * @param {number} last - past-the-end index to search.
+ * @param {number} val - value to compare the elements to.
+ * @param {function} [comp] - custom comparator.
+ * @return {bool} whether [first, last) contains `val`
+ */
+function binarySearch (arr, first, last, val, comp = less) {
+  const it = exports.lowerBound.apply(this, arguments)
+  if (it < last && arr[it] === val) return true
+  return false
+}
+ 
+/**
+ * Given an ordered range, finds the subrange in which all elements are equivalent to a given value.
+ * @param {Array} arr - array to search
+ * @param {number} first - beginning index to search
+ * @param {number} last - past-the-end index to search.
+ * @param {number} val - value to compare the elements to.
+ * @param {function} [comp] - custom comparator.
+ * @return {[number, number]} whether [first, last) contains `val`
+ */
+function equalRange (arr, first, last, val, comp = less) {
+  return [
+    exports.lowerBound.apply(this, arguments),
+    exports.upperBound.apply(this, arguments)
+  ]
+}
+ 
+module.exports = {
+  less,
+  greater,
+  lowerBound,
+  upperBound,
+  equalRange,
+  binarySearch
+}
