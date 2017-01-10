2b5688a5228c2181749fbf01fcc28c3d11c8af06
flowmemo <flowmemo@outlook.com>
2017-01-10 22:38:20 +0800

    fix testcase

--- test/fixtures/011.js
+++ test/fixtures/011.js
@@ -1,7 +1,12 @@
+// 11. Container With Most Water
 const data = [
   {
     input: [[1, 8, 6, 2, 5, 4, 8, 3, 7]],
-    ans: 4
+    ans: 49
+  },
+  {
+    input: [[1, 2, 13, 14, 5, 6]],
+    ans: 18
   }
 ]
 
