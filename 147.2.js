40bce041e96498b72eb4cef79dc52f5cd3664b84
flowmemo <flowmemo@outlook.com>
2017-01-13 12:33:16 +0800

    147. Insertion Sort List

---
+++ 147.2.js
@@ -0,0 +1,42 @@
+/*
+147. Insertion Sort List
+Sort a linked list using insertion sort.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function (head) {
+  'use strict'
+  let dummy = { next: null }
+  let tail = dummy
+  while (head) {
+    if (head.val >= tail.val) {
+      tail.next = head
+      tail = head
+      head = head.next
+      continue
+    }
+    let nhead = head.next
+    let prev = dummy
+    let cur = dummy.next
+    while (cur && cur.val < head.val) {
+      prev = cur
+      cur = cur.next
+    }
+    prev.next = head
+    head.next = cur
+    head = nhead
+  }
+  return dummy.next
+}
+
+module.exports = insertionSortList
---
+++ test/fixtures/147.js
@@ -0,0 +1,29 @@
+// 147. Insertion Sort List
+const {buildFromArray, convertToArray} = require('../LinkedList.js')
+
+const data = [
+  {
+    input: [[2, 4, 3, 5, 6, 4]],
+    ans: [2, 3, 4, 4, 5, 6]
+  },
+  {
+    input: [[1, 1, 1]],
+    ans: [1, 1, 1]
+  },
+  {
+    input: [[]],
+    ans: []
+  }
+]
+
+const options = {
+  inProcessor: input => input.map(buildFromArray),
+  outProcessor: output => convertToArray(output)
+}
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker,
+  options
+}
+
