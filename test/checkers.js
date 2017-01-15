870c511d1a36701fce35cfa205b5ac98a8ee0230
flowmemo <flowmemo@outlook.com>
2017-01-15 22:46:47 +0800

    add processor for ans

--- test/checkers.js
+++ test/checkers.js
@@ -2,16 +2,17 @@
 const assert = require('assert')
 const util = require('util')
 const cloneDeep = require('lodash').cloneDeep
-
+const I = x => x
 function normalChecker (fsolve, data, options = {}) {
-  const inProcessor = options.inProcessor || (d => d)
-  const outProcessor = options.outProcessor || (d => d)
+  const inProcessor = options.inProcessor || I
+  const outProcessor = options.outProcessor || I
+  const ansProcessor = options.ansProcessor || I
   for (const {input, ans} of data) {
     const originInput = cloneDeep(input)
     const output = outProcessor(fsolve(...inProcessor(input)))
     const errMsg = 'input:\n' + util.inspect(originInput, {colors: true}) + '\noutput:\n' +
     util.inspect(output, {colors: true}) + '\nans:\n' + util.inspect(ans, {colors: true})
-    assert.deepStrictEqual(output, ans, errMsg)
+    assert.deepStrictEqual(output, ansProcessor(ans), errMsg)
   }
 }
 
