f87ee02a401cc12c46bac7579e34ee7f8e29861b
flowmemo <flowmemo@outlook.com>
2017-01-20 21:54:27 +0800

    441. Arranging Coins

---
+++ test/fixtures/441.js
@@ -0,0 +1,17 @@
+// 441. Arranging Coins
+
+const data = [
+  {
+    input: [5],
+    ans: 2
+  },
+  {
+    input: [8],
+    ans: 3
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
