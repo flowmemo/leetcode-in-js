7c036ff8971009e42314711721624c364870ec97
flowmemo <flowmemo@outlook.com>
2017-02-01 23:57:49 +0800

    448. Find All Numbers Disappeared in an Array

---
+++ test/fixtures/448.js
@@ -0,0 +1,13 @@
+// 448. Find All Numbers Disappeared in an Array
+
+const data = [
+  {
+    input: [[4, 3, 2, 7, 8, 2, 3, 1]],
+    ans: [5, 6]
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
