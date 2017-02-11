8fd0100f55b729bc77a0f713b24cc88d51441929
flowmemo <flowmemo@outlook.com>
2017-02-11 23:26:47 +0800

    436. Find Right Interval

---
+++ test/fixtures/436.js
@@ -0,0 +1,31 @@
+// 436. Find Right Interval
+
+const data = [
+  {
+    input: [[ [1, 2] ]],
+    ans: [-1]
+  },
+  {
+    input: [[ [3, 4], [2, 3], [1, 2] ]],
+    ans: [-1, 0, 1]
+  },
+  {
+    input: [[ [1, 4], [2, 3], [3, 4] ]],
+    ans: [-1, 2, -1]
+  }
+]
+
+const options = {
+  inProcessor: input => [input[0].map(v => new Interval(v[0], v[1]))]
+}
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker,
+  options
+}
+
+function Interval (start, end) {
+  this.start = start
+  this.end = end
+}
