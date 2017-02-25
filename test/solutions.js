4841af0015f4392b7c62f8070d81ebb3e4d34e7a
flowmemo <flowmemo@outlook.com>
2017-02-25 23:59:02 +0800

    add stats

--- test/solutions.js
+++ test/solutions.js
@@ -1,9 +1,10 @@
-const assert = require('assert')
 const fs = require('fs')
 const validFile = /^\d{3}(\.\d+)?\.js$/
 
 fs.readdir('./', (err, allFiles) => {
   if (err) throw err
+  let fail = 0
+  let stat = Object.create(null)
   const files = allFiles.filter(file => validFile.test(file))
   for (let file of files) {
     const id = file.slice(0, 3)
@@ -13,7 +14,13 @@ fs.readdir('./', (err, allFiles) => {
       const solution = require(`../${file}`)
       checker(solution, data, options)
     } catch (err) {
-      console.error(file + ' false')
+      fail++
+      const errType = err.name
+      if (!stat[errType]) stat[errType] = []
+      stat[errType].push(file)
+      console.log(file + ' ' + errType)
     }
   }
+  console.log(stat)
+  console.log(fail)
 })
