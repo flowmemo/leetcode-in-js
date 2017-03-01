a28d6f80e00dd0cdfc1c135b2ea1d37494961d12
flowmemo <flowmemo@outlook.com>
2017-03-01 18:34:08 +0800

    change path

--- test/check.js
+++ test/check.js
@@ -5,5 +5,5 @@ const file = process.argv[2]
 const filepath = path.resolve(process.cwd(), file)
 const solution = require(filepath)
 const id = path.basename(file).slice(0, 3)
-const {data, checker, options} = require(`./fixtures/${id}.js`)
+const {data, checker, options} = require(`./solution/${id}.js`)
 checker(solution, data, options)
