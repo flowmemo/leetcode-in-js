641bd93b075c16e0923d19e4f04dba19fc016b64
flowmemo <flowmemo@outlook.com>
2017-03-14 00:36:26 +0800

    476. Number Complement

---
+++ test/solution/476.js
@@ -0,0 +1,17 @@
+// 476. Number Complement
+
+const data = [
+  {
+    input: [5],
+    ans: 2
+  },
+  {
+    input: [1],
+    ans: 0
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
