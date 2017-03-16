e9ee0288170eb592483a4136b944bbe64030e88b
flowmemo <flowmemo@outlook.com>
2017-03-17 00:40:25 +0800

    225. Implement Stack using Queues (no test)

---
+++ solution/225.js
@@ -0,0 +1,76 @@
+/*
+225. Implement Stack using Queues
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Notes:
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and all test cases.
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function () {
+  'use strict'
+  this.q = []
+}
+
+/**
+ * Push element x onto stack.
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  'use strict'
+  const q = this.q
+  const len = q.length
+  q.push(x)
+  for (let i = 0; i < len; ++i) {
+    q.push(q.shift())
+  }
+}
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  'use strict'
+  const q = this.q
+  return q.shift()
+}
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  'use strict'
+  const q = this.q
+  return q[0]
+}
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  'use strict'
+  const q = this.q
+  return q.length === 0
+}
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = Object.create(MyStack).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
