0d7896aa3d8d25c666f7cd9fdbb18a55d0d2c1c0
flowmemo <flowmemo@outlook.com>
2017-03-20 23:35:47 +0800

    454. 4Sum II

---
+++ test/solution/454.js
@@ -0,0 +1,13 @@
+// 454. 4Sum II
+
+const data = [
+  {
+    input: [[1, 2], [-2, -1], [-1, 2], [0, 2]],
+    ans: 2
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
