4ee8bda0369ed5dd67ef6077c61b0162e5f269e6
flowmemo <flowmemo@outlook.com>
2017-04-09 17:22:41 +0800

    557. Reverse Words in a String III

---
+++ test/solution/557.js
@@ -0,0 +1,13 @@
+// 557. Reverse Words in a String III
+
+const data = [
+  {
+    input: ["Let's take LeetCode contest"],
+    ans: "s'teL ekat edoCteeL tsetnoc"
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
