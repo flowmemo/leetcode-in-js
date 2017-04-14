d6315d603bc092f349b1161e48bd02702df9efb4
flowmemo <flowmemo@outlook.com>
2017-04-15 00:00:22 +0800

    492. Construct the Rectangle

---
+++ test/solution/492.js
@@ -0,0 +1,17 @@
+// 492. Construct the Rectangle
+
+const data = [
+  {
+    input: [4],
+    ans: [2, 2]
+  },
+  {
+    input: [6],
+    ans: [3, 2]
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
