ee9d9286b0f1f648b33f5d4bde2d702f63819856
flowmemo <flowmemo@outlook.com>
2017-04-26 23:17:41 +0800

    191. Number of 1 Bits

---
+++ solution/191.js
@@ -0,0 +1,27 @@
+/*
+191. Number of 1 Bits
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+*/
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  'use strict'
+  let mask = 1 << 31
+  let ret = 0
+
+  while (mask) {
+    if (mask & n) ret++
+    mask >>>= 1
+  }
+  return ret
+}
+
+module.exports = hammingWeight
---
+++ test/solution/191.js
@@ -0,0 +1,13 @@
+// 191. Number of 1 Bits
+
+const data = [
+  {
+    input: [[11]],
+    ans: 3
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
