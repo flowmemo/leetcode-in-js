2e95717c22ba34916255a392bdb2d5bd37274faf
flowmemo <flowmemo@outlook.com>
2017-05-17 00:11:08 +0800

    60. Permutation Sequence

--- test/solution/060.js
+++ test/solution/060.js
@@ -1,6 +1,17 @@
-module.exports = [
+// 55. Jump Game
+
+const data = [
   {
-    input: [3, 5],
-    ans: '312'
+    input: [3, 3],
+    ans: '213'
+  },
+  {
+    input: [9, 6575],
+    ans: '135289746'
   }
 ]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
