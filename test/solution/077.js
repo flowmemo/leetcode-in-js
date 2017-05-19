33c7ea56efbab323cb7f7d902721759bcb1c2b8d
flowmemo <flowmemo@outlook.com>
2017-05-19 23:16:21 +0800

    77. Combinations

---
+++ test/solution/077.js
@@ -0,0 +1,36 @@
+// 77. Combinations
+
+const data = [
+  {
+    input: [4, 2],
+    ans: [
+      [2, 4],
+      [3, 4],
+      [2, 3],
+      [1, 2],
+      [1, 3],
+      [1, 4]
+    ]
+  }
+]
+
+function sort2dArray (arr) {
+  for (let line of arr) {
+    line.sort((a, b) => a - b)
+  }
+  return arr.sort()
+}
+
+for (let { ans } of data) {
+  sort2dArray(ans)
+}
+
+const options = {
+  outProcessor: sort2dArray
+}
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker,
+  options
+}
