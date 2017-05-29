359c0dfb70243b03cec6961ea4c2d3441ea7a35d
flowmemo <flowmemo@outlook.com>
2017-05-29 19:42:34 +0800

    565. Array Nesting

---
+++ test/solution/565.js
@@ -0,0 +1,13 @@
+// 565. Array Nesting
+
+const data = [
+  {
+    input: [[5, 4, 0, 3, 1, 6, 2]],
+    ans: 4
+  }
+]
+
+module.exports = {
+  data,
+  checker: require('../checkers.js').normalChecker
+}
