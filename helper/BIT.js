06f8685165d6f08637a0439b9702c637a5f3f7ff
flowmemo <flowmemo@outlook.com>
2018-01-28 14:15:40 +0800

    Binary Indexed Tree

---
+++ helper/BIT.js
@@ -0,0 +1,34 @@
+// Binary Indexed Tree
+
+class BIT {
+  constructor (arr) {
+    const n = this.n = arr.length
+    this.tree = Array(n + 1).fill(0)
+    for (let i = 0; i < n; ++i) {
+      this.addAtIndex(i, arr[i])
+    }
+  }
+  addAtIndex (i, val) {
+    const tree = this.tree
+    const n = this.n
+    i += 1
+    while (i <= n) {
+      tree[i] += val
+      i += i & (-i)
+    }
+  }
+  sumPrefix (endOffset) {
+    const tree = this.tree
+    let sum = 0
+    while (endOffset) {
+      sum += tree[endOffset]
+      endOffset -= endOffset & (-endOffset)
+    }
+    return sum
+  }
+  sumRange (startOffset, endOffset) {
+    return this.sumPrefix(endOffset) - this.sumPrefix(startOffset)
+  }
+}
+
+module.exports = BIT
